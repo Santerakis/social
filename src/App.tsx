b082cbd5a6fc3db0d188a500f7d982a7219a15d9
Sergey <santerakis@gmail.com>
2023-02-03 21:11:23 +0200

    10_lesson

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from "./Header";
 import Technologies from "./Technologies";
+import Footer from "./Footer";
 
 function App() {
     return (
-        <div className="App">
+        <div>
             <Header/>
             <Technologies/>
+            <Footer/>
         </div>
     );
 }
