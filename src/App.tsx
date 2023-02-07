8ee5df240c93bad66c558caa532d19e7dc76fa2b
Sergey <santerakis@gmail.com>
2023-02-07 22:52:43 +0200

    grid_start

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import './App.css';
 import Header from "./Header";
-import Technologies from "./Technologies";
-import Footer from "./Footer";
+
 
 function App() {
     return (
         <div>
-            <Header/>
-            <Technologies/>
-            <Footer/>
+            <header>
+                <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQizIk2O6mWu1dJoC9sMk-ftZEgmlGr_ToB6w&usqp=CAU'/>
+            </header>
+            <nav>
+                <div>
+                    Profile
+                </div>
+                <div>
+                    Messages
+                </div>
+            </nav>
+            <div>
+                Main content
+            </div>
         </div>
     );
 }
