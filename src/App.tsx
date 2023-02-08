c0152fa57fa530d04ba3c41e6b372e45f578edb3
Sergey <santerakis@gmail.com>
2023-02-08 21:57:46 +0200

    completed_11_lesson

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,43 @@ import Header from "./Header";
 
 function App() {
     return (
-        <div>
-            <header>
-                <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQizIk2O6mWu1dJoC9sMk-ftZEgmlGr_ToB6w&usqp=CAU'/>
+        <div className='app-wrapper'>
+            <header className='header'>
+                <img src='https://upload.wikimedia.org/wikipedia/commons/4/4f/Twitter-logo.svg'/>
             </header>
-            <nav>
+            <nav className='nav'>
                 <div>
-                    Profile
+                    <a>Profile</a>
                 </div>
                 <div>
-                    Messages
+                    <a>Messages</a>
+                </div>
+                <div>
+                    <a>New</a>
+                </div>
+                <div>
+                    <a>Music</a>
+                </div>
+                <div>
+                    <a>Settings</a>
                 </div>
             </nav>
-            <div>
-                Main content
+            <div className='content'>
+                <div><img src='https://oboinastol.net/katalog_kartinok/tom16/032/skachat_oboi_1920x1080.jpg'/></div>
+                <div>ava + description</div>
+                <div>
+                    My posts
+                <div>New post</div>
+                </div>
+                <div>
+                    <div>
+                        post1
+                    </div>
+                    <div>
+                        post2
+                        post2
+                    </div>
+                </div>
             </div>
         </div>
     );
