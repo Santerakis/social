6e69fa435b2d7aa7087d86555f443f29b9175271
Sergey <santerakis@gmail.com>
2023-02-08 22:33:11 +0200

    completed_12_lesson

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,16 @@
 import React from 'react';
 import './App.css';
-import Header from "./Header";
-
+import Header from "./Components/Header";
+import Navbar from "./Components/Navbar";
+import Profile from "./Components/Profile";
 
 function App() {
     return (
         <div className='app-wrapper'>
-            <header className='header'>
-                <img src='https://upload.wikimedia.org/wikipedia/commons/4/4f/Twitter-logo.svg'/>
-            </header>
-            <nav className='nav'>
-                <div>
-                    <a>Profile</a>
-                </div>
-                <div>
-                    <a>Messages</a>
-                </div>
-                <div>
-                    <a>New</a>
-                </div>
-                <div>
-                    <a>Music</a>
-                </div>
-                <div>
-                    <a>Settings</a>
-                </div>
-            </nav>
-            <div className='content'>
-                <div><img src='https://oboinastol.net/katalog_kartinok/tom16/032/skachat_oboi_1920x1080.jpg'/></div>
-                <div>ava + description</div>
-                <div>
-                    My posts
-                <div>New post</div>
-                </div>
-                <div>
-                    <div>
-                        post1
-                    </div>
-                    <div>
-                        post2
-                        post2
-                    </div>
-                </div>
-            </div>
+            <Header/>
+            <Navbar/>
+            <Profile/>
+
         </div>
     );
 }
---
+++ src/Components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://upload.wikimedia.org/wikipedia/commons/4/4f/Twitter-logo.svg'/>
+        </header>
+    );
+};
+
+export default Header;
