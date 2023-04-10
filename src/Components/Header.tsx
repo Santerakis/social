b53e0910cd9fa53209021fd66abe221e9371e05a
Sergey stava <santerakis@gmail.com>
2023-04-10 21:04:37 +0300

    step

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
     return (
         <header className='header'>
-            <img src='https://upload.wikimedia.org/wikipedia/commons/4/4f/Twitter-logo.svg'/>
+            <img src='https://upload.wikimedia.org/wikipedia/commons/c/ce/Twitter_Logo.png?20210909091158'/>
         </header>
     );
 };
